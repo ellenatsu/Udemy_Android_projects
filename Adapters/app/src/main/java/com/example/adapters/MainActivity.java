2a21a5aa5fe5160283b2db3693ccde2b26f556e1
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-02-13 15:55:45 -0800

    add customize listview

--- Adapters/app/src/main/java/com/example/adapters/MainActivity.java
+++ Adapters/app/src/main/java/com/example/adapters/MainActivity.java
@@ -3,11 +3,31 @@ package com.example.adapters;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    //data source
+    String[] worldcup = {
+            "germany",
+            "spain",
+            "japan",
+            "argentina"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listview);
+
+        //adapter
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.adapterlist, R.id.textView, worldcup);
+        listView.setAdapter(adapter);
+
     }
 }
