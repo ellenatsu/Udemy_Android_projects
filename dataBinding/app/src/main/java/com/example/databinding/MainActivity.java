81579bcce2d8c1f301362fbad522680984846a24
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-02-16 15:44:45 -0800

    create paging app

--- dataBinding/app/src/main/java/com/example/databinding/MainActivity.java
+++ dataBinding/app/src/main/java/com/example/databinding/MainActivity.java
@@ -2,23 +2,29 @@ package com.example.databinding;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<mActivity> extends AppCompatActivity {
     public FloatingActionButton fab;
+    private Activity mActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mActivity = this;
+
         fab = (FloatingActionButton) findViewById(R.id.floatingbtn);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                Intent i = new Intent(mActivity, AddNewContactActivity.class);
+                startActivity(i);
             }
         });
     }
