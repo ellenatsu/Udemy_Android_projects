c0b02ce51e30842abb5f6ab1e12733558e9b80c2
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-02-26 02:15:55 -0800

    init everything in mainactivity

---
+++ PagingApp/app/src/main/java/com/example/pagingapp/activity/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.pagingapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.bumptech.glide.RequestManager;
+import com.example.pagingapp.R;
+import com.example.pagingapp.adapter.MoviesAdapter;
+import com.example.pagingapp.adapter.MoviesLoadStateAdapter;
+import com.example.pagingapp.databinding.ActivityMainBinding;
+import com.example.pagingapp.util.GridSpace;
+import com.example.pagingapp.util.MovieComparator;
+import com.example.pagingapp.util.Utils;
+import com.example.pagingapp.viewmodel.MovieViewModel;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+import okhttp3.internal.Util;
+
+@AndroidEntryPoint
+public class MainActivity extends AppCompatActivity {
+
+    MovieViewModel mainActivityViewModel;
+    ActivityMainBinding binding;
+    MoviesAdapter moviesAdapter;
+
+    @Inject
+    RequestManager requestManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        if(Utils.API_KEY == null || Utils.API_KEY.isEmpty()){
+            Toast.makeText(this, "Error in API KEY", Toast.LENGTH_LONG).show();
+        }
+        moviesAdapter = new MoviesAdapter(new MovieComparator(), requestManager);
+        mainActivityViewModel = new ViewModelProvider(this).get(MovieViewModel.class);
+
+        initRecyclerviewAndAdapter();
+
+        //subscribe to paging data
+        mainActivityViewModel.moviePagingDataFlowable.subscribe(moviePagingData -> {
+            moviesAdapter.submitData(getLifecycle(), moviePagingData);
+        });
+    }
+
+    private void initRecyclerviewAndAdapter() {
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        binding.recyclerViewMovies.setLayoutManager(gridLayoutManager);
+        binding.recyclerViewMovies.addItemDecoration(new GridSpace(2, 20, true));
+        binding.recyclerViewMovies.setAdapter(
+                moviesAdapter.withLoadStateFooter(
+                        new MoviesLoadStateAdapter(view -> {
+                            moviesAdapter.retry();
+                        }
+                        )
+                )
+        );
+        gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                return moviesAdapter.getItemViewType(position) == MoviesAdapter.LOADING_ITEM ? 1 : 2;
+            }
+        });
+    }
+}
