a06fa17b860803d7a072932d403e99954a2c3757
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-02-28 22:54:44 -0800

    add keys

--- FirestoreApp/app/src/main/java/com/example/firestoreapp/MainActivity.java
+++ FirestoreApp/app/src/main/java/com/example/firestoreapp/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.firestoreapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Firebase Firestore:
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 }
