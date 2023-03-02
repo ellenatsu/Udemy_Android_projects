84566145eb0803cffe2e32cb3aecf06e944def81
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-03-02 15:06:01 -0800

    add activity

--- FirestoreApp/app/src/main/java/com/example/firestoreapp/MainActivity.java
+++ FirestoreApp/app/src/main/java/com/example/firestoreapp/MainActivity.java
@@ -1,32 +1,34 @@
 package com.example.firestoreapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Firebase Firestore:
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    Button loginBtn;
+    Button createAccBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loginBtn = findViewById(R.id.login);
+        createAccBtn = findViewById(R.id.create_acc);
 
+        createAccBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 }
